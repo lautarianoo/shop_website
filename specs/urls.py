9baed4f82d2e8a78fb690123e97f1799c0cd7435
lautarianoo <neonchick1>
2021-06-02 22:27:59 +0500

    add category specs

--- specs/urls.py
+++ specs/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', BaseSpecView.as_view(), name='base_spec')
+    path('', BaseSpecView.as_view(), name='base_spec'),
+    path('add-category/', NewCategoryView.as_view(), name='add_category')
 ]
--- specs/views.py
+++ specs/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import NewCategoryForm
+from django.http import HttpResponseRedirect
 
 class BaseSpecView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'specs/product_features.html', {})
+
+class NewCategoryView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = NewCategoryForm(request.POST or None)
+        return render(request, 'specs/new_category.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = NewCategoryForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/product-specs/')
+        return  render(request, 'specs/new_category.html', {'form': form})
